377cff946bacb3e820ffac146d1241365c5383b2
Joe Hanson <johanson104@gmail.com>
2018-11-16 14:01:24 -0800

    fix path

--- precache-manifest.b8ec1458b0dee8f81290f72474d2bf6c.js
+++ precache-manifest.b8ec1458b0dee8f81290f72474d2bf6c.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
     "revision": "229c360febb4351a89df",
-    "url": "/static/js/runtime~main.229c360f.js"
+    "url": "/ColorChanger/static/js/runtime~main.229c360f.js"
   },
   {
     "revision": "abe7cdef7528c5d00112",
-    "url": "/static/js/main.abe7cdef.chunk.js"
+    "url": "/ColorChanger/static/js/main.abe7cdef.chunk.js"
   },
   {
     "revision": "ee222cbe01c9c22645f3",
-    "url": "/static/js/1.ee222cbe.chunk.js"
+    "url": "/ColorChanger/static/js/1.ee222cbe.chunk.js"
   },
   {
     "revision": "abe7cdef7528c5d00112",
-    "url": "/static/css/main.9ea46a5e.chunk.css"
+    "url": "/ColorChanger/static/css/main.9ea46a5e.chunk.css"
   },
   {
     "revision": "1798c63c6b9378207641f9ddf26ff044",
